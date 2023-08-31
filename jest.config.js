2cd316e313dfc1ff25907fa1bf489390a5fd3bd6
HyeonMin Shin <gusals121234@gmail.com>
2023-08-31 15:33:53 +0900

    init: docs & main folder add

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   preset: 'react-native',
+  // This is needed to load CanvasKit
+  globalSetup: '@shopify/react-native-skia/globalJestSetup.js',
+  // This is needed to load the mock
+  setupFiles: ['@shopify/react-native-skia/jestSetup.js'],
 };
